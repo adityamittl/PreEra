c5c671b2f23d0dae88b17a20f9b055ce36e90fc1
Aditya Mittal <aditmttl@gmail.com>
2021-09-12 02:43:33 +0530

    bug fix

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -7,5 +7,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
-    path('', include('authentication.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
